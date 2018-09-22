1c3e9f7f29d28403c97af5bf138bb1dcfd613180
Paul Gamble <pgamble@gmail.com>
2018-09-22 13:27:07 -0400

    added main.js

--- llc-intro-to-browser-extensions-master/first-extension/script.js
+++ llc-intro-to-browser-extensions-master/first-extension/script.js
@@ -8,12 +8,13 @@ chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
    }]);
  });
 
+
 // This is the action for the click for our Chrome extension
  chrome.pageAction.onClicked.addListener(function() {
  	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
  		chrome.tabs.executeScript(
  			tabs[0].id,
- 			{code:  'document.body.style.backgroundColor = "green";' }
+ 			{file: 'main.js'}
  		);
  	});
  });
